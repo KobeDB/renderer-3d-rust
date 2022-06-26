9dc12d6b4fcac97371d3f4c56b9ac8afae609e4d
Kobe De Broeck <kobe.debroeck1@gmail.com>
2022-06-26 13:01:01 +0200

    transformations on vectors

--- src/matrix4.rs
+++ src/matrix4.rs
@@ -1,15 +1,13 @@
-use std::intrinsics::cosf32;
-
 /// we use row vectors
 pub struct Matrix4 {
-    elements: [[f32; 4]; 4],
+    pub elements: [[f32; 4]; 4],
 }
 
 impl Matrix4 {
     pub fn new_identity() -> Self {
         let mut elements = [[0.0 as f32; 4]; 4];
 
-        for i in 0..3 {
+        for i in 0..=3 {
             elements[i][i] = 1.0;
         }
 
@@ -20,10 +18,10 @@ impl Matrix4 {
     pub fn new_rotation_z(angle_rad: f32) -> Self {
         let mut result = Self::new_identity();
 
-        elements[0][0] = f32::cos(angle_rad);
-        elements[0][1] = -f32::sin(angle_rad);
-        elements[1][0] = f32::sin(angle_rad);
-        elements[1][1] = f32::cos(angle_rad);
+        result.elements[0][0] = f32::cos(angle_rad);
+        result.elements[0][1] = -f32::sin(angle_rad);
+        result.elements[1][0] = f32::sin(angle_rad);
+        result.elements[1][1] = f32::cos(angle_rad);
 
         result
     }
@@ -32,15 +30,27 @@ impl Matrix4 {
     pub fn new_rotation_x(angle_rad: f32) -> Self {
         let mut result = Self::new_identity();
 
-        elements[0][0] = f32::cos(angle_rad);
-        elements[0][1] = -f32::sin(angle_rad);
-        elements[1][0] = f32::sin(angle_rad);
-        elements[1][1] = f32::cos(angle_rad);
+        result.elements[1][1] = f32::cos(angle_rad);
+        result.elements[1][2] = -f32::sin(angle_rad);
+        result.elements[2][1] = f32::sin(angle_rad);
+        result.elements[2][2] = f32::cos(angle_rad);
 
         result
     }
 
-    pub fn mul_mut(&mut self, other: &Self) {
+    pub fn mul(&self, other: &Self) -> Self {
+        let mut elements = [[0.0 as f32; 4]; 4];
 
+        for col in 0..=3 {
+            for row in 0..=3 {
+                let mut dot_prod: f32 = 0.0;
+                for i in 0..=3 {
+                    dot_prod += self.elements[row][i] * other.elements[i][col];
+                }
+                elements[row][col] = dot_prod;
+            }
+        }
+
+        Self{elements}
     }
 }
--- src/vec4.rs
+++ src/vec4.rs
@@ -1,21 +1,43 @@
-pub struct Vec4 {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
-    pub w: f32
-}
+use crate::matrix4::Matrix4;
+
+pub struct Vec4 { elems: [f32;4] }
 
 impl Vec4 {
+
+    pub fn x(&self) -> f32 { self.elems[0] }
+    pub fn y(&self) -> f32 { self.elems[1] }
+    pub fn z(&self) -> f32 { self.elems[2] }
+    pub fn w(&self) -> f32 { self.elems[3] }
+
     /// Creates a 4D point, used to represent 3D positions.
     /// The 4th component is the homogeneous coordinate used for translations.
     pub fn new_point(x: f32, y: f32, z: f32) -> Self {
-        Vec4 { x, y, z, w: 1.0 }
+        Self{elems: [ x, y, z, 1.0 ]}
     }
 
     /// Creates a 4D vector.
     /// Contrary to point, translations do not affect this vector.
     /// Thus vector is an ideal way of representing directions.
     pub fn new_vec4(x: f32, y: f32, z: f32) -> Self {
-        Vec4 { x, y, z, w: 0.0 }
+        Self{elems: [ x, y, z, 0.0 ]}
     }
+
+    pub fn transform(&mut self, mat: &Matrix4) {
+        for el in 0..=3 {
+            let mut new_el: f32 = 0.0;
+            for i in 0..=3 {
+                new_el += mat.elements[i][el];
+            }
+            new_el *= self.elems[el];
+            self.elems[el] = new_el;
+        }
+    }
+}
+
+fn dot_product(u: &[f32;4], v: &[f32;4]) -> f32 {
+    let mut result = 0.0;
+    for i in 0..=3 {
+        result += u[i]*v[i];
+    }
+    result
 }
